49d7ebef4413cdc231032dfcf83ab1326e4aacc9
Jesús David Cantillo Romero <jesusdavidcantilloromero.10@gmail.com>
2026-02-11 22:42:30 -0500

    Se reestructuraron los componentes debido al uso de React Router

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,18 @@
+import Arqueros from './Arqueros';
+import Defensas from './Defensas';
+import Volantes from './Volantes';
+import Delanteros from './Delanteros';
+
+function Home() {
+  return (
+    <>
+      <h1>Selección Colombia 2014</h1>
+      <Arqueros />
+      <Defensas />
+      <Volantes />
+      <Delanteros />
+    </>
+  );
+}
+
+export default Home;
