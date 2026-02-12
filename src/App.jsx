8d92e269bfa90d1ffd0ae5b3fdb62cd1d33a5401
Jesús David Cantillo Romero <jesusdavidcantilloromero.10@gmail.com>
2026-02-11 21:38:49 -0500

    Actualización de estilos y índice de App, importación de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Arqueros from "./Arqueros";
+import Defensas from "./Defensas";
+import Volantes from "./Volantes";
+import Delanteros from "./Delanteros";
+
+const images = {
+  ospina: new URL("./img/ospina.jpg", import.meta.url).href,
+  vargas: new URL("./img/vargas.jpg", import.meta.url).href,
+  josecuadrado: new URL("./img/josecuadrado.jpg", import.meta.url).href,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Selección Colombia 2018</h1>
+      <Arqueros images={images} />
+      <Defensas images={images} />
+      <Volantes images={images} />
+      <Delanteros images={images} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
